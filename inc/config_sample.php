34be294489b3f21c780ad1d3ad153fdca300f73a
Matthew Reidsma <matthew@reidsrow.com>
2014-10-09 22:19:05 -0400

    Added sample config file

---
+++ inc/config_sample.php
@@ -0,0 +1,12 @@
+<?php 
+
+// Database Connection Details
+$db_host = 'localhost'; // Database host. Probably is localhost, but might be something else.
+$db_user = ''; // Database user's name
+$db_pass = ''; // Database user's password
+$db_database = ''; // Name of the database
+
+// Organization Name
+$org_name = 'My Library'; 
+
+
